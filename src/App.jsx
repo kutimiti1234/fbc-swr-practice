7a7abc68a6eded80bd9ab580818829a183c2a5d9
kutimiti1234 <tarbit.offensive@gmail.com>
2025-01-05 19:50:28 +0900

    fix: move fetcher definition outside of app function to avoid re-definition on re-renders

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import useSWR from "swr";
 
+const headers = { Accept: "application/json" };
+const fetcher = (url) => fetch(url, { headers }).then((res) => res.json());
+
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
-  const headers = { Accept: "application/json" };
-
-  const fetcher = (url) => fetch(url, { headers }).then((res) => res.json());
   const { data, error, isLoading } = useSWR(url, fetcher);
 
   if (isLoading) return <div>Loading...</div>;
